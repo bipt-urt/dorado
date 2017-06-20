3f9e6af72b19332e4fa4d938c6d4b2cf3aa59a00
DGideas <dgideas@outlook.com>
2017-06-20 10:55:39 +0800

    较小的修复和改进

---
+++ dorado.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <codecvt>
+#include <locale>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	fstream files=("1.c",std::ios::in);
+	u32stream line;
+	for()
+
+	return 0;
+}
