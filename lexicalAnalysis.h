489563f2053e7b74c5d0576bdc977aa03214f90a
VTvow <824221253@qq.com>
2017-06-26 15:58:08 +0800

    修改lexicalAnalysis.h

---
+++ lexicalAnalysis.h
@@ -0,0 +1,18 @@
+void toBlock(vector<u16string>& line)
+{
+	vector<u16string> saveBlock;
+	u16string tempBlock;
+	for (auto element: line)
+	{
+		if (element[0] == u'v')
+		{
+			while(1)
+			{
+				for(auto ele:element)
+				{
+					
+				}
+			}
+		}
+	}
+}
