759cbc7922d05470b4c20af2d8a4e3be6513d407
VTvow <824221253@qq.com>
2017-06-27 11:13:22 +0800

    增加participle文件

---
+++ participle.h
@@ -0,0 +1,20 @@
+struct word
+{
+	u16string _word;
+	u16string _type;
+}
+bool typeWord()
+{
+	
+}
+vector<word> wordSegment(const u16string& line)
+{
+	vector<word> getWordAndType;
+	word._word getWord;
+	word._type getType;
+	for(auto element:line)
+	{
+		if()
+	}
+	
+}
